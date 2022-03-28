0b522505339987bb1bbb8ba056d4ec0db5845fc2
coderkhayrul <coderkhayrul@gmail.com>
2022-03-28 17:31:08 +0600

    SideBar Update

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -17,16 +17,190 @@
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="bx bx-store"></i>
-                        <span key="t-ecommerce">Ecommerce</span>
+                        <i class="bx bx-list-ul"></i>
+                        <span key="t-ecommerce">Product</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="ecommerce-products.html" key="t-products">Products</a></li>
-                        <li><a href="ecommerce-product-detail.html" key="t-product-detail">Product Detail</a></li>
-                        <li><a href="ecommerce-orders.html" key="t-orders">Orders</a></li>
-                        <li><a href="ecommerce-customers.html" key="t-customers">Customers</a></li>
+                        <li><a href="#" >Category</a></li>
+                        <li><a href="#" >Product List</a></li>
+                        <li><a href="#" >Add Product</a></li>
+                        <li><a href="#" >Print Barcode</a></li>
+                        <li><a href="#" >Adjustment List</a></li>
+                        <li><a href="#" >Add Adjustment</a></li>
+                        <li><a href="#" >Stock Count</a></li>
                     </ul>
                 </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-credit-card"></i>
+                        <span key="t-purchase">Purchase</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" >Purchase List</a></li>
+                        <li><a href="#" >Add Purchase</a></li>
+                        <li><a href="#" >Import Purchase By CSV</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-cart-alt"></i>
+                        <span key="t-purchase">Sale</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" >Sell List</a></li>
+                        <li><a href="#" >POS</a></li>
+                        <li><a href="#" >Import Sell By CSV</a></li>
+                        <li><a href="#" >Gift Card List</a></li>
+                        <li><a href="#" >Coupon List</a></li>
+                        <li><a href="#" >Delivery List</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-wallet-alt"></i>
+                        <span key="t-expense">Expense</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Expense Category</a></li>
+                        <li><a href="#">Expense List</a></li>
+                        <li><a href="#">Add Expense</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-detail"></i>
+                        <span key="t-quatation">Quatation</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Quatation List</a></li>
+                        <li><a href="#">Add Quatation</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-share"></i>
+                        <span key="t-transfer">Transfer</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Transfer List</a></li>
+                        <li><a href="#">Add Transfer</a></li>
+                        <li><a href="#">Import Transfer By CSV</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-rotate-left"></i>
+                        <span key="t-return">Return</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Sale</a></li>
+                        <li><a href="#">Purchase</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-briefcase-alt-2"></i>
+                        <span key="t-return">Accounting</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Accounting List</a></li>
+                        <li><a href="#">Add Accounting</a></li>
+                        <li><a href="#">Money Transfer</a></li>
+                        <li><a href="#">Balance Sheet</a></li>
+                        <li><a href="#">Account Statement</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="dripicons-user-group"></i>
+                        <span key="t-return">HRM</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Department</a></li>
+                        <li><a href="#">Employee</a></li>
+                        <li><a href="#">Attendance</a></li>
+                        <li><a href="#">Payroll</a></li>
+                        <li><a href="#">Holiday</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-user"></i>
+                        <span key="t-return">People</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">User List</a></li>
+                        <li><a href="#">Add User</a></li>
+                        <li><a href="#">Customer List</a></li>
+                        <li><a href="#">Add Customer</a></li>
+                        <li><a href="#">Biller List</a></li>
+                        <li><a href="#">Add Biller</a></li>
+                        <li><a href="#">Supplier List</a></li>
+                        <li><a href="#">Add Supplier</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-file"></i>
+                        <span key="t-return">Report</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Summary Report</a></li>
+                        <li><a href="#">Best Seller</a></li>
+                        <li><a href="#">Product Report</a></li>
+                        <li><a href="#">Daily Sale</a></li>
+                        <li><a href="#">Monthly Sale</a></li>
+                        <li><a href="#">Daily Purchase</a></li>
+                        <li><a href="#">Monthly Purchase</a></li>
+
+                        <li><a href="#">Sale Report</a></li>
+                        <li><a href="#">Payment Report</a></li>
+                        <li><a href="#">Purchase Report</a></li>
+                        <li><a href="#">Warehouse Report</a></li>
+                        <li><a href="#">Warehouse Stock Chart</a></li>
+                        <li><a href="#">Product Quatation Alert</a></li>
+                        <li><a href="#">User Report</a></li>
+                        <li><a href="#">Customer Report</a></li>
+                        <li><a href="#">Supplier Report</a></li>
+                        <li><a href="#">Due Report</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-cog"></i>
+                        <span key="t-return">Setting</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#">Role Permission</a></li>
+                        <li><a href="#">Descount Plan</a></li>
+                        <li><a href="#">Discount</a></li>
+                        <li><a href="#">Send Notification</a></li>
+                        <li><a href="#">Warehouse</a></li>
+                        <li><a href="#">Customer Group</a></li>
+                        <li><a href="#">Brand</a></li>
+                        <li><a href="#">Unit</a></li>
+                        <li><a href="#">Currency</a></li>
+                        <li><a href="#">Tax</a></li>
+                        <li><a href="#">User Profile</a></li>
+                        <li><a href="#">Create SMS</a></li>
+                        <li><a href="#">Backup Database</a></li>
+                        <li><a href="#">General Setting</a></li>
+                        <li><a href="#">Mail Setting</a></li>
+                        <li><a href="#">Reward Point Setting</a></li>
+                        <li><a href="#">SMS Setting</a></li>
+                        <li><a href="#">POS Setting</a></li>
+                        <li><a href="#">HRM Setting</a></li>
+                    </ul>
+                </li>
+
                 <li>
                     <a href="{{ route('logout') }}"  onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
                         <i class="bx bx-log-in-circle text-danger"></i>
