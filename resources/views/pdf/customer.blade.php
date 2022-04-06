20fb4f9cda6c3069c79691d2eb0a802151ded345
coderkhayrul <coderkhayrul@gmail.com>
2022-04-06 14:42:36 +0600

    Invoice Customer  Desing  Update

--- resources/views/pdf/customer.blade.php
+++ resources/views/pdf/customer.blade.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html xmlns='https://www.w3.org/1999/xhtml'>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Customer List</title>
-</head>
-<body>
-    .
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+
+<body style='font-family:Tahoma;font-size:12px;color: #333333;background-color:#FFFFFF;'>
+    <table align='center' border='0' cellpadding='0' cellspacing='0' style='height:842px; width:595px;font-size:12px;'>
+        <tr>
+            <td valign='top'>
+                <table width='100%' height='100' cellspacing='0' cellpadding='0'>
+                    <tr>
+                        <td>
+                            <div align='center'
+                                style='font-size: 20px; font-weight: bold; background:#1282eb; color:#FFFFFF'>Customer
+                                List </div>
+                        </td>
+                    </tr>
+                </table>
+                <table width='100%' cellspacing='0' cellpadding='2' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+
+                        <td width='15%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:12px;'><strong>Name
+                            </strong></td>
+                        <td bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:12px;'><strong>Phone</strong></td>
+                        <td width='20%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:12px;'><strong>Company Name</strong>
+                        </td>
+                        <td bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:12px;'><strong>Remarks</strong></td>
+                        <td bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:12px;'><strong>Address</strong></td>
+
+                    </tr>
+                    <tr style="display:none;">
+                        <td colspan="*">
+                    <tr>
+
+                        <td valign='top' style='font-size:12px;'>my product</td>
+                        <td valign='top' style='font-size:12px;'>3 </td>
+                        <td valign='top' style='font-size:12px;'>115.00 </td>
+                        <td valign='top' style='font-size:12px;'>345.00 </td>
+
+                    </tr>
+            </td>
+        </tr>
+    </table>
+    </td>
+    </tr>
+    </table>
 </body>
+
 </html>
